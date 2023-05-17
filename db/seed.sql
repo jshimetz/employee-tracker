6f688d878af542fc3b71791d875b817a2ffca46f
jshimetz <jeremyshimetz@gmail.com>
2023-05-17 15:38:23 -0500

    Adding SQL code

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO departments (name) VALUES
+  ('Human Resources'),
+  ('Finance'),
+  ('Sales'),
+  ('Marketing');
+
+INSERT INTO employees (first_name, last_name, email, address, phone_number, department) VALUES
+  ('John', 'Doe', 'john.doe@example.com', '123 Main St, City', '123-456-7890', 'Human Resources'),
+  ('Jane', 'Smith', 'jane.smith@example.com', '456 Elm St, City', '987-654-3210', 'Finance'),
+  ('Mike', 'Johnson', 'mike.johnson@example.com', '789 Oak St, City', '555-123-4567', 'Sales');
